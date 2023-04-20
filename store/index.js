01d8c25710d79e76d4374608cfc2fd885401a19b
ariel levi <ariel22411@gmail.com>
2023-04-20 19:59:42 +0300

    asd

--- store/index.js
+++ store/index.js
@@ -12,9 +12,24 @@ export default {
   },
 
   actions: {
-    // async nuxtServerInit ({ dispatch, state }) {
+    async nuxtServerInit ({ dispatch, state }) {
     //   dispatch('global/autoLogin')
-    // }
+    try {
+      const cookieStr = process.browser ? document.cookie : this.app.context.req.headers.cookie
+      const cookies = Cookie.parse(cookieStr || '') || {}
+      const token = cookies['auth._token.local']
+      if (token && token !== 'false') {
+        Cookies.set('token', token)
+        const user = await this.$userService.getUserByToken({ token })
+        // const customer = await this.$customersService.getCustomer(token)
+        if (user) { 
+          await this.$auth.setUser(user)
+        }
+      }
+    } catch (e) {
+      console.log('autoLogin error', e)
+    }
+    }
   },
 
   getters: {
